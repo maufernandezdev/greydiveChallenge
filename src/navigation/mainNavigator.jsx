08d33f38c11cbebb2f4a4df6cc6482dd7e99de91
Mauricio Fernandez <mfernandezuk@hotmail.com>
2023-01-05 17:04:48 -0300

    fixed bug in not found import

--- src/navigation/mainNavigator.jsx
+++ src/navigation/mainNavigator.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Forms from '../pages/Forms';
-import NotFound from '../pages/notFound';
+import NotFound from '../pages/NotFound';
 
 const mainNavigator = () => {
     return (
